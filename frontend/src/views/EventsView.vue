60e4012c758d82e3f0e90177c4df9646d65ff1d7
cyzuwia <e5277@outlook.com>
2025-04-13 00:16:51 +0800

    创建 EventsView.vue

---
+++ frontend/src/views/EventsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="events-view">
+    <h2>事件管理</h2>
+    <EventManager />
+  </div>
+</template>
+
+<script>
+import EventManager from '../components/EventManager.vue'
+
+export default {
+  name: 'EventsView',
+  components: {
+    EventManager
+  }
+}
+</script>
+
+<style scoped>
+.events-view h2 {
+  margin-bottom: 20px;
+  font-size: 24px;
+  font-weight: 500;
+}
+</style>
