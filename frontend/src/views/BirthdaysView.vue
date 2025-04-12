cdac0d789200eae1f2650b3efe4c01726046823e
cyzuwia <e5277@outlook.com>
2025-04-13 00:16:47 +0800

    创建 BirthdaysView.vue

---
+++ frontend/src/views/BirthdaysView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="birthdays-view">
+    <h2>生日提醒</h2>
+    <BirthdayPage />
+  </div>
+</template>
+
+<script>
+import BirthdayPage from '../components/BirthdayPage.vue'
+
+export default {
+  name: 'BirthdaysView',
+  components: {
+    BirthdayPage
+  }
+}
+</script>
+
+<style scoped>
+.birthdays-view h2 {
+  margin-bottom: 20px;
+  font-size: 24px;
+  font-weight: 500;
+}
+</style>
