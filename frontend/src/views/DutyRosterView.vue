9c6bfbf5d2d40789dfe94f1c74201a825eb2a908
cyzuwia <e5277@outlook.com>
2025-04-13 00:16:49 +0800

    创建 DutyRosterView.vue

---
+++ frontend/src/views/DutyRosterView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="duty-roster-view">
+    <h2>值日表</h2>
+    <DutyRosterPage />
+  </div>
+</template>
+
+<script>
+import DutyRosterPage from '../components/DutyRosterPage.vue'
+
+export default {
+  name: 'DutyRosterView',
+  components: {
+    DutyRosterPage
+  }
+}
+</script>
+
+<style scoped>
+.duty-roster-view h2 {
+  margin-bottom: 20px;
+  font-size: 24px;
+  font-weight: 500;
+}
+</style>
